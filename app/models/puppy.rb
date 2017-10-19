4bf256af7e9ff5f1e8f35676915b981afd05a68e
Carmen <carmenhchung@gmail.com>
2017-10-19 17:32:14 +1100

    added css and homepage image, edited navbar, created home controller

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -1,5 +1,10 @@
 class Puppy < ApplicationRecord
+<<<<<<< HEAD
   belongs_to :user
+=======
+  include ImageUploader[:image]
+  # belongs_to :user
+>>>>>>> 48ea7b2... added css and homepage image, edited navbar, created home controller
 
   # Favorited by users
   has_many :favorite_puppies # just the 'relationships'
