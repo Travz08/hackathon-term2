4f32cb3fdbdab937f496b2915bfdcf661ba67927
Young Jae Nam <Jaenam@Young-Jae-Nams-MacBook-Air.local>
2017-10-19 13:12:57 +1100

    first commit

---
+++ db/migrate/20171019013320_create_puppies.rb
@@ -0,0 +1,17 @@
+class CreatePuppies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :puppies do |t|
+      t.string :name
+      t.string :age
+      t.string :breed
+      t.string :size
+      t.string :gender
+      t.text :bio
+      t.boolean :desexed
+      t.boolean :vaccinated
+      t.integer :adoption_fee
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/system/puppies_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class PuppiesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit puppies_url
+  #
+  #   assert_selector "h1", text: "Puppy"
+  # end
+end
