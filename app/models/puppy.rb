ff8b786e926df0c22bdf9575e2a23a8299d3dbde
J1marotta <jim_is55@hotmail.com>
2017-10-20 11:45:43 +1100

    search works on all params

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -7,7 +7,8 @@ class Puppy < ApplicationRecord
   has_many :favorited_by, through: :favorite_puppies, source: :user #
 
   def self.search(search)
-    where("breed LIKE ?", "%#{search}%")
-  end 
+    where("name LIKE ? OR breed LIKE ? OR age LIKE ? OR size LIKE ? OR gender LIKE ? OR bio LIKE ? OR desexed LIKE ? OR vaccinated LIKE ? OR adoption_fee LIKE ?", "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%")
+
+  end
 
 end
