ba7be2c2a00167f23061068cff3e7b812c8e60d8
Travis Kelekolio <traviskelekolio@yahoo.com>
2017-10-20 14:36:07 +1100

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,22 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+
+  config.action_mailer.default_url_options = { host: 'pawesome.herokuapp.com'}
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain    => ENV['MAILGUN_DOMAIN'],
+    :user_name => ENV['MAILGUN_USERNAME'],
+    :password  => ENV['MAILGUN_PASSWORD'],
+  }
+
+
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "hackathon-term2_#{Rails.env}"
